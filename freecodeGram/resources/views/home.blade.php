acd366cdde6c1574a0850a076f930f5290edcda1
fb0801 <fbhatti0801@gmail.com>
2023-07-07 19:34:49 +0100

    designing ui from insta 2

--- freecodeGram/resources/views/home.blade.php
+++ freecodeGram/resources/views/home.blade.php
@@ -2,21 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+`   <img  src="https://scontent-lhr8-2.cdninstagram.com/v/t51.2885-19/97566921_2973768799380412_5562195854791540736_n.jpg?stp=dst-jpg_s320x320&_nc_ht=scontent-lhr8-2.cdninstagram.com&_nc_cat=104&_nc_ohc=EW5oaf0I4mYAX_hUGuq&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfBxH5EVJ7GGL4hhVfNBZ6afUnoplKRyB3goDCDutrzhjA&oe=64AE2A9F&_nc_sid=8b3546" class="rounded-circle">
+        </div>
+        <div class="col-9">
+            
         </div>
     </div>
 </div>
