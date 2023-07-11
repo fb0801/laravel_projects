4a920c43b5622012a9a89d65360e7e0e4f49eb67
fb0801 <fbhatti0801@gmail.com>
2023-07-11 15:59:09 +0100

    add posts to db and more 2

--- freecodeGram/routes/web.php
+++ freecodeGram/routes/web.php
@@ -20,5 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/p/create', 'PostsController@create');
+Route::get('/p', 'PostsController@store');
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
