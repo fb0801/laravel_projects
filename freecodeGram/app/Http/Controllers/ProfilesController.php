4dbcd5ec0262f3ff55d8c8c16b3063e5fc1829ff
fb0801 <fbhatti0801@gmail.com>
2023-07-13 18:13:19 +0100

    edit profile 2

--- freecodeGram/app/Http/Controllers/ProfilesController.php
+++ freecodeGram/app/Http/Controllers/ProfilesController.php
@@ -1,18 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Http\Request;
+
 
 class ProfilesController extends Controller
 {
-    public function index($user)
+    public function index(App\User $user)
     {
-        $user = User::findOrFail($user);
-        return view('profiles.index', [
-            'user' => $user
-        ]);
+        return view('profiles.index', compact('user'));
     }
+
+    public function edit(User $user){
+
+        return view('profiles.edit', compact('user'));
+
+    }
+
+    public function update(User $user){
+        $data = request()->validate([
+            'title'=> 'required',
+            'description' => 'required',
+            'url' =>'url',
+            'image' => '',
+        ]);
+
+        dd($data);
+    }
+
 }
 
